34732636c0559aac1ad14f06bcb3f47bc8595e6a
enlightenone <stevenwchow@gmail.com>
2016-02-27 23:46:19 -0800

    Aligning bottoms on front page

--- js/sliders.js
+++ js/sliders.js
@@ -1,18 +1,17 @@
 window.onload = function(){
-  var image_id  =  "image_" + "1";
-  var imageNode  = document.getElementById(image_id);
+  // This is to initiate sliders to display first image.
+  var imageNode  = document.getElementById("image_1");
   imageNode.style.display = "block";
-
+  Slides.sliders = document.getElementById("images_slides").children;
 }
 
+
+/*********** Image interval display function ********************/
 var image_length = 6 ;
 var flag = 1;
 var highlighted_image = "image_" + flag;
-var image_id;
-var image_node;
 
- myVar = setInterval(function(){ changesSliders() }, 3000);
-
+var initInterval = setInterval(function(){ changesSliders() }, 3000);
 
 function changesSliders() {
      if (flag <= 6 ){
@@ -22,6 +21,7 @@ function changesSliders() {
         highlighted_image = "image_1";
         flag = 1;
      }
+
     for (i=1 ; i <= 6 ; i++) {
        var image_id = "image_" + i;
        if(image_id == highlighted_image ){
@@ -29,67 +29,61 @@ function changesSliders() {
        } else {
           document.getElementById(image_id).style.display = "none";
        }
+    } // End of for loop functon
+} // End of changesSlider() function
 
-    }
-}
+/*********** Image interval display function ********************/
+
+/************* Sliders Control functions ***********************/
 
 function stopSliders(){
-  clearInterval(myVar);
-}
+  clearInterval(initInterval);
+} // End of stopSliders() function
 
 function startSliders(){
-  myVar = setInterval(function(){ changesSliders() }, 3000);
-}
+  initInterval = setInterval(function(){ changesSliders() }, 3000);
+} // End of startSliders() function
 
-function nextSlide(){
-  var images_block = document.getElementById("images_slides");
-  var sliders  = images_block.children;
-  var display_attribute, next_image_id;
+var Slides = {
+              nextImage: function(){
+                            var display_attribute, next_image_id;
+                            for(i=0; i < 6; i++){
+                                display_attribute = this.sliders[i].style.display;
+                           
+                             if (display_attribute == "block"){
+                                if (i == 5){
+                                  this.sliders[0].style.display = "block";
+                                } else {
+                                  next_image_id = i + 1;
+                                }
+                              } // end of if conditional statement
 
-  for(i=0; i < 6; i++){
-      display_attribute = sliders[i].style.display;
- 
-   if (display_attribute == "block"){
-      if (i == 5){
-        sliders[0].style.display = "block";
-      } else {
-        next_image_id = i + 1;
-      }
-    } // end of if conditional statement
+                             if(i === next_image_id){
+                                this.sliders[i].style.display = "block";
+                              } else {
+                                this.sliders[i].style.display = "none";
+                              }
+                            } // End of for loop block
+                        }, // End of  nextImage function
+                prevImage: function(){
+                            var display_attribute, prev_image_id;
+                            for(i=5; i >= 0; i--){
+                                display_attribute = this.sliders[i].style.display;
+                             if (display_attribute == "block"){
+                                if (i == 0){
+                                  this.sliders[5].style.display = "block";
+                                } else {
+                                  prev_image_id = i - 1;
+                                }
+                              } // end of if conditional statement
 
-   if(i === next_image_id){
-      sliders[i].style.display = "block";
-    } else {
-       sliders[i].style.display = "none";
-    }
-  } // ENd of for loop block
- } // End of nextSlide() function
+                             if(i === prev_image_id){
+                                this.sliders[i].style.display = "block";
+                              } else {
+                                this.sliders[i].style.display = "none";
+                              }
+                            } // End of for loop block
+                         } // End of prevImage() function
+              } // End of Slides object
 
-
-function prevSlide(){
-  var images_block = document.getElementById("images_slides");
-  var sliders  = images_block.children;
-  var display_attribute, next_image_id;
-
-  for(i=5; i >= 0; i--){
-      display_attribute = sliders[i].style.display;
- 
-   if (display_attribute == "block"){
-      if (i == 0){
-        sliders[5].style.display = "block";
-      } else {
-        next_image_id = i - 1;
-      }
-    } // end of if conditional statement
-
-   if(i === next_image_id){
-      sliders[i].style.display = "block";
-    } else {
-       sliders[i].style.display = "none";
-    }
-  } // ENd of for loop block
-
-
-
-} // End of prevSlide() function
-
+/************* End of Sliders Control functions ***********************/
